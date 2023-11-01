b1c3503432d9b71dbdcd0dc1bd98d04c1cf42beb
gorsicleo <gorsicleo@gmail.com>
2023-11-01 17:17:53 +0100

    Chess greedy algorithm

--- chess/ChessAIEngine.py
+++ chess/ChessAIEngine.py
@@ -1,9 +1,45 @@
 import random
 
-
+piece_score = {"K":0, "Q":10, "R":5, "B":3, "N":3, "p":1}
+checkmate = 1000
+stalemate = 0
 def find_random_move(valid_moves):
     return valid_moves[random.randint(0, len(valid_moves) - 1)]
 
 
-def find_best_move():
-    return
+def find_best_move(game_state, valid_moves):
+    opponent_min_max_score = checkmate
+    best_move = None
+    best_player_move = None
+    random.shuffle(valid_moves)
+    turn_multiplier = 1 if game_state.white_to_move else -1
+    for player_move in valid_moves:
+        game_state.make_move(player_move)
+        opponent_moves = game_state.get_valid_moves()
+        opponent_max_score = -checkmate
+        for opponent_move in opponent_moves:
+            game_state.make_move(opponent_move)
+            if game_state.check_mate:
+                score = -turn_multiplier * checkmate
+            elif game_state.stale_mate:
+                score = stalemate
+            else:
+                score = -turn_multiplier * score_material(game_state.board)
+            if score > opponent_max_score:
+                opponent_max_score = score
+            game_state.undo_move()
+        if opponent_max_score < opponent_min_max_score:
+            opponent_min_max_score = opponent_max_score
+            best_player_move = player_move
+        game_state.undo_move()
+    return  best_player_move
+
+def score_material(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == "w":
+                score += piece_score[square[1]]
+            elif square[0] == "b":
+                score -= piece_score[square[1]]
+    return score
--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import pygame
+pygame.init()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+Create program window
+"""
+
+window = pygame.display.set_mode((500, 500))
+background = (200, 255, 255)
+window.fill(background)
+clock = pygame.time.Clock()
+
+"""
+Create card class
+"""
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Card:
+    rect = pygame.Rect(0, 0, 10, 10)
+    fill_color = "blue"
+
+    def __init__(self, x, y, width, height, color):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.fill_color = color
+
+    def change_color(self, new_color):
+        self.fill_color = new_color
+
+    def fill(self):
+        pygame.draw.rect(window, self.fill_color, self.rect)
+
+    def outline(self, color, thickness):
+        pygame.draw.rect(window, color, self.rect, thickness)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Label(Card):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self, x, y, width, height, color):
+        super().__init__(x, y, width, height, color)
+        self.image = None
+
+    def set_text(self, text, font_size, text_color):
+        self.image = pygame.font.SysFont("verdana", font_size).render(text, True, text_color)
+
+    def draw(self, shift_x, shift_y):
+        self.fill()
+        window.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+
